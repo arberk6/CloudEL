d656c47fba073ff10105e1dd9727faae3b655de1
medi <meedii_11@hotmail.com>
2019-02-07 18:50:52 +0100

    connection with db

---
+++ CEL/DAL/Generals.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DAL
+{
+    public static class Generals
+    {
+        static public SqlConnection GetNewConnection()
+        {
+            string conStr = ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+
+            SqlConnection con = new SqlConnection(conStr);
+            try
+            {
+                con.Open();
+                return con;
+            }
+            catch (Exception ex)
+            {
+                con.Close();
+                throw new Exception("Lidhja me Databazen nuk mund te realizohet");
+            }
+        }
+    }
+}
