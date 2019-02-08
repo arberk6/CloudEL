2e8b50b76467759acd138b6d458a23de1baf2a6b
arberk6 <arkadriu@gmail.com>
2019-02-08 04:14:06 +0100

    mappers: studenti, profesori, personi

--- CEL/DAL/StudentiMapper.cs
+++ CEL/DAL/StudentiMapper.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using BLL;
+
 namespace DAL
+  
 {
     public class StudentiMapper
     {
-          
+          (new Studenti()).
     }
 }
